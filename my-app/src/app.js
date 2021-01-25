028857b0ec5b5c0d6078eeb126b865955416d426
andreamazza84 <andrea.mazza84@gmail.com>
2021-01-25 17:17:21 +0100

    Sass funzionante

---
+++ my-app/src/app.js
@@ -0,0 +1,10 @@
+//Installate nodejs
+//2. usate Laravelmix per creare un progetto SASS
+//3. create una semplice index.html con degli h2, h3 e paragrafi
+//4. applicate agli h2 e h3 lo stesso colore tramite una variabile.
+
+console.log("ciao");
+let user = {
+    name: 'Andrea',
+    surname: 'Mazza'
+};
